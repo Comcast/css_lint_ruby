c899104c45760b128c6ff11ebd5832fb6e50bc87
Mike Ball <mikedball@gmail.com>
2012-10-10 13:46:55 -0400

    Corrected typos in spec file

--- spec/lib/css_lint_spec.rb
+++ spec/lib/css_lint_spec.rb
@@ -1,55 +1,54 @@
 require 'spec_helper'
 
-describe Compass do
-  
+describe CSSLint do
   describe "Lint" do
     describe "#new" do
       context "it is provided a valid argument" do
         before(:each) do
           File.stub!(:join).and_return('/some_fake_dir')
-          @cl = Compass::Lint.new 'foo'
+          @cl = CSSLint::Lint.new 'foo'
         end
 
-        it "creates and instance of Compass::Lint if it's passed an argument" do
-          @cl.instance_of?(Compass::Lint).should == true
+        it "creates an instance of CSSLint::Lint if it's passed an argument" do
+          @cl.instance_of?(CSSLint::Lint).should == true
         end
 
-        it "sets the value of @css to an array of css sources passed to the compass-lint command, relative to the current working directory" do
+        it "sets the value of :css to an array of css sources passed to the csslint command, relative to the current working directory" do
           @cl.css.class == Array
           @cl.css.should == ['foo']
         end
 
-        it "sets the value of @gem_vendor_dir" do
+        it "sets the value of :gem_vendor_dir" do
           @cl.gem_vendor_dir.should == '/some_fake_dir'
         end
       end
-
+      
       context "it is not provided a valid argument" do
         before(:each) do
-          @cl = Compass::Lint.new
+          @cl = CSSLint::Lint.new
         end
-
+        
         it "does not fail if no argument is provided" do
-          @cl.instance_of?(Compass::Lint).should == true
+          @cl.instance_of?(CSSLint::Lint).should == true
         end
 
-        it "sets the value of @css to the value of the error_message method" do
+        it "sets the value of :css to the value of the error_message method" do
           @cl.css.should == @cl.error_message
         end
       end
     end
-
+    
     describe "#error_message" do
       it "returns the string 'Must provide compass-lint a CSS file or directory'" do
-        @cl = Compass::Lint.new
-        @cl.error_message.should == "Must provide compass-lint a CSS file or directory"
+        @cl = CSSLint::Lint.new
+        @cl.error_message.should == "Must provide csslint a CSS file or directory"
       end
     end
-
+    
     describe "#execute" do
       context "Lint is not instantiated with a valid argument" do
-        it "prints the value of @css, which should be the value of error_message" do
-          @cl = Compass::Lint.new
+        it "prints the value of :css, which should be the value of error_message" do
+          @cl = CSSLint::Lint.new
           $stdout.should_receive(:puts).with(@cl.error_message)
           @cl.execute
         end
@@ -57,7 +56,7 @@ describe Compass do
 
       context "Lint is instantiated with a valid argument" do
         it "calls run_lint" do
-          @cl = Compass::Lint.new 'foo'
+          @cl = CSSLint::Lint.new 'foo'
           @cl.stub!(:run_lint)
           @cl.should_receive(:run_lint)
           @cl.execute
@@ -67,16 +66,16 @@ describe Compass do
 
     describe "#get_java_path" do
       it "calls system(which java)" do
-        @cl = Compass::Lint.new 'foo'
+        @cl = CSSLint::Lint.new 'foo'
         @cl.should_receive(:'`').with("which java")
         @cl.get_java_path
       end
     end
-
+    
     describe "#run_lint" do
       context "the user does not have java installed" do
         it "raises 'You do not have a Java installed, but it is required.'" do
-          @cl = Compass::Lint.new 'foo'
+          @cl = CSSLint::Lint.new 'foo'
           @cl.stub!(:get_java_path).and_return(false)
           @cl.should_receive(:get_java_path)
           lambda{@cl.run_lint}.should raise_exception
@@ -84,9 +83,9 @@ describe Compass do
       end
 
       context "the user has java installed" do
-        it "lints the value of @css" do
+        it "lints the value of :css" do
           File.stub!(:join).and_return('/fake_vendor_dir')
-          @cl = Compass::Lint.new 'foo'
+          @cl = CSSLint::Lint.new 'foo'
           @cl.stub!(:get_java_path).and_return('java installed')
           @cl.should_receive(:get_java_path)
           @cl.should_receive("system").with("java -jar /fake_vendor_dir/js.jar /fake_vendor_dir/csslint-rhino.js $@ foo")
